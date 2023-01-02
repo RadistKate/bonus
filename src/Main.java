6dde7275be7c4c71ee500cac461aa568caca34e7
Katy test <artebyakinae@mail.ru>
2023-01-02 15:04:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 8900;
+        int milya = 20;
+
+        System.out.println("При покупке билета вы получаете:");
+        System.out.println(( cost / milya )  + " бонусов" );
+
+    }
+}
